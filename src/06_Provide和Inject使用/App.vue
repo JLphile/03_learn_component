4d25e15070248518a882144f185a047aad5aeec0
JLphile <lkyz001@outlook.com>
2022-09-03 21:11:14 +0800

    master

---
+++ src/06_Provide和Inject使用/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <Home></Home>
+    <button @click="addName">添加名字</button>
+  </div>
+</template>
+
+<script>
+import Home from './Home.vue';
+import { computed } from 'vue';
+
+export default {
+  components: {
+    Home,
+  },
+  provide() {
+    return {
+      name: 'why',
+      age: 18,
+      length: computed(() => this.names.length),
+    };
+  },
+  data() {
+    return {
+      names: ['aaa', 'bbb', 'ccc', 'ddd'],
+    };
+  },
+  methods: {
+    addName() {
+      this.names.push('why');
+      console.log(this.names);
+    },
+  },
+};
+</script>
+
+<style scoped></style>
---
+++ src/06_Provide和Inject使用/HomeContent.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>HomeContent的内容: {{ name }}-{{ age }}-{{ length }}</div>
+</template>
+
+<script>
+export default {
+  inject: ['name', 'age', 'length'],
+};
+</script>
+
+<style scoped></style>
