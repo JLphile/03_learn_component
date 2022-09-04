3288a3c5dedae47409ad2cbb04484e4da731bb40
JLphile <lkyz001@outlook.com>
2022-09-04 21:40:31 +0800

    master

---
+++ src/08_插槽的基本使用/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <my-slot-cpn>
+      
+    </my-slot-cpn>
+  </div>
+</template>
+
+<script>
+import MySlotCpn from './MySlotCpn.vue';
+export default {
+  components: { MySlotCpn },
+};
+</script>
+
+<style scoped></style>
