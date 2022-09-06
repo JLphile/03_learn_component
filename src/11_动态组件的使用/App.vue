b37682bd7bb13421c053980c6e9cc2354e68f13b
JLphile <lkyz001@outlook.com>
2022-09-06 21:00:29 +0800

    master

---
+++ src/11_动态组件的使用/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <button
+      v-for="item in tabs"
+      :key="item"
+      @click="itemClick(item)"
+      :class="{ active: currentTab === item }"
+    >
+      {{ item }}
+    </button>
+    <template v-if="currentTab === 'home'">
+      <home></home>
+    </template>
+    <template v-if="currentTab === 'about'">
+      <about></about>
+    </template>
+    <template v-if="currentTab === 'category'">
+      <category></category>
+    </template>
+  </div>
+</template>
+
+<script>
+import Home from './pages/Home.vue';
+import About from './pages/About.vue';
+import Category from './pages/Category.vue';
+export default {
+  components: {
+    Home,
+    About,
+    Category,
+  },
+  data() {
+    return {
+      tabs: ['home', 'about', 'category'],
+      currentTab: 'home',
+    };
+  },
+  methods: {
+    itemClick(item) {
+      this.currentTab = item;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.active {
+  color: red;
+}
+</style>
