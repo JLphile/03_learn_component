caad60daec6ee6a732462533421b2d079a3c32bc
Jlphile <lkyz001@outlook.com>
2022-09-07 10:57:51 +0800

    14-creted

---
+++ src/14_组件的生命周期/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h2>App组件</h2>
+    <home></home>
+  </div>
+</template>
+
+<script>
+import Home from './Home.vue';
+export default {
+  components: {
+    Home,
+  },
+};
+</script>
+
+<style scoped></style>
